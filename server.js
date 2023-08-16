3e0b6dcc2ae6f31bf38080a9fb19b24e63241f10
zara sarafraz <zarasrf0@gmail.com>
2023-08-16 15:25:38 +0330

    creating routes and responses

--- server.js
+++ server.js
@@ -5,7 +5,30 @@ const dotenv =require('dotenv')
 dotenv.config({ path: './config/config.env'})
 
 const app = express()
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({success: true, msg: 'show all bootcamps' })
+
+})
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({success: true, msg: 'show bootcamp' })
+
+})
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({success: true, msg: 'create new bootcamp' })
+
+})
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({success: true, msg: `update bootcamp ${req.params.id}` })
+
+})
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({success: true, msg: `delete bootcamp ${req.params.id}`  })
+
+})
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(
+  PORT,
+  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
