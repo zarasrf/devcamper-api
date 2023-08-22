1a55a13e26c6e4d4417eb2fe894df8aad7c244f4
zara sarafraz <zarasrf0@gmail.com>
2023-08-22 12:58:12 +0330

    done section 5- geojson location & geocoder hook

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose')
 const slugify = require('slugify')
+const geocoder = require('../utils/geocoder')
 
-const BootcampSchema = new mongoose.Schema({
+const BootcampSchema = new mongoose.Schema(
+    {
     name: {
         type: String,
-        required: [true, 'plese add a name'],
+        required: [true, 'please add a name'],
         unique: true,
-        trim:true,
+        trim: true,
         maxlength: [50,'Name can not be more than 50 characters']
     },
     slug: String,
@@ -39,17 +41,14 @@ const BootcampSchema = new mongoose.Schema({
     },
     location: {
         // GeoJSON Point
-        type:{
-            type: String,
-        enum: ['Point'],
-
-        // required: true,
-    },
-    coordinates: {
-        type: [Number],
-        // required: true ,
-        index: '2dsphere'
-    },
+        type: {
+          type: String,
+          enum: ['Point']
+        },
+        coordinates: {
+          type: [Number],
+          index: '2dsphere'
+        },
     formattedAddress: String,
     street: String,
     city: String,
@@ -104,9 +103,46 @@ const BootcampSchema = new mongoose.Schema({
 
 // create bootcamp slug from the name
 BootcampSchema.pre('save',function(next){
-    this.slug = slugify(this.name, { lowe: true })
-    console.log('Slugify ran', this.name)
+    this.slug = slugify(this.name, { lower: true })
+    // console.log('Slugify ran', this.name)
     next()
 })
+
+
+// Geocode & create location field
+BootcampSchema.pre('save', async function(next) {
+    const loc = await geocoder.geocode(this.address)
+    this.location = {
+        type: 'Point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress,
+        street: loc[0].streetName,
+        city: loc[0].city,
+        state: loc[0].stateCode,
+        zipcode: loc[0].zipcode,
+        country: loc[0].countryCode,
+    }
+
+    // Do ot save address in DB
+    this.address = undefined
+    next()
+}
+)
  
+// Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function(next) {
+    console.log(`Courses being removed from bootcamp ${this._id}`);
+    await this.model('Course').deleteMany({ bootcamp: this._id });
+    next();
+  });
+  
+  // Reverse populate with virtuals
+  BootcampSchema.virtual('courses', {
+    ref: 'Course',
+    localField: '_id',
+    foreignField: 'bootcamp',
+    justOne: false
+  });
+  
+
 module.exports = mongoose.model('Bootcamp', BootcampSchema)
