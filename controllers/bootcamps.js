cb661687e4e4436c401a296bcc5aa10cdd8cd0f5
zara sarafraz <zarasrf0@gmail.com>
2023-08-22 16:16:23 +0330

    get radius loading...

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,6 +1,8 @@
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
+const geocoder = require('../utils/geocoder')
 const Bootcamp = require('../models/Bootcamp')
+
 
 // @desc     Get all bootcamps
 // @route    Get/api/v1/bootcamps
@@ -75,7 +77,32 @@ exports.deleteBootcamp =asyncHandler(async (req, res, next) => {
 })
 
 
+// @desc     get bootcamp whitin a darius
+// @route    GET/api/v1/bootcamps/radius/:zipcode/:distance
+// acess     Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+    const { zipcode, distance } = req.params
 
+    // Get lat/lng from geocoder
+    const loc = await geocoder.geocode(zipcode)
+    const lat = loc[0].latitude
+    const lng = loc[0].longitude
+
+    // Calc radius using radians
+    // Divide dist by radius of Earth
+    // Earth Radius = 3,963 mi / 6,378 km
+    const radius = distance / 3963
+
+    const bootcamps = await Bootcamp.find({
+        location: { $geoWithin: { $centerSphere: [ [ lng,lat ], radius ] } }
+    })
+
+    res.status(200).json({
+        success: true,
+        count: bootcamps.length,
+        data: bootcamps
+    })
+})
 
 
 
