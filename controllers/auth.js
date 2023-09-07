104eab6752915d9651703c8e7472437573d725f7
zara sarafraz <zarasrf0@gmail.com>
2023-09-07 09:41:25 +0330

    sendingJWT in a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,10 +16,12 @@ exports.register = asyncHandler(async (req, res, next) => {
         role
     })
 
-    // Create token
-    const token = user.getSignedJwtToken()
+    sendTokenResponse(user,200, res)
 
-    res.status(200).json({ success: true, token })
+    // Create token
+    // const token = user.getSignedJwtToken()
+
+    // res.status(200).json({ success: true, token })
 })
 // @desc     Login user
 // @route    POST/api/v1/auth/auth/login
@@ -45,9 +47,34 @@ exports.login = asyncHandler(async (req, res, next) => {
     if(!isMatch) {
         return next(new ErrorResponse('Invalid credentials', 401))
     }
+
+    sendTokenResponse(user, 200, res)
     
+    // // Create token
+    // const token = user.getSignedJwtToken()
+
+    // res.status(200).json({ success: true, token })
+})
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
     // Create token
     const token = user.getSignedJwtToken()
 
-    res.status(200).json({ success: true, token })
-})
+    const options = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    }
+
+    if(process.env.NODE_ENV ==='production') {
+        options.secure = true
+    }
+
+    res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({
+        success: true,
+        token
+    })
+}
