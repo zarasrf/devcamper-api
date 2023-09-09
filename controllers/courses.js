8405ddf8ddcaaee9a94a01e7ad996d6e6d32a6a8
zara sarafraz <zarasrf0@gmail.com>
2023-09-09 17:56:20 +0330

    courses ownership

--- controllers/courses.js
+++ controllers/courses.js
@@ -45,12 +45,15 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 })
 
 
-// @desc     Add a course 
+// @desc     Add course 
 // @route    POST/api/v1/bootcamp/:bootcampId/courses
 // acess     Private
 exports.addCourse = asyncHandler(async (req, res, next) => {
 
     req.body.bootcamp = req.params.bootcampId
+    req.body.user = req.user.id
+
+
 
     const bootcamp = await Bootcamp.findById(req.params.bootcampId)
   
@@ -58,6 +61,11 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
       return next(new ErrorResponse( `No bootcamp with the id of ${req.params.bootcampId}`), 404)
   }
 
+  // Make sure  user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+        new ErrorResponse(`User ${req.user.id} is not authorized to add a course to this ${bootcamp._id}`, 401))
+}
   const course = await Course.create(req.body)
   
       res.status(200).json({
@@ -76,6 +84,12 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
     if (!course) {
       return next(new ErrorResponse( `No bootcamp with the id of ${req.params.id}`), 404)
   }
+
+  // Make sure  user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+        new ErrorResponse(`User ${req.user.id} is not authorized to update a course ${course._id}`, 401))
+}
 
   course = await Course.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -100,6 +114,13 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
   }
 
 //   await course.remove()
+
+  // Make sure  user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+        new ErrorResponse(`User ${req.user.id} is not authorized to delete a course ${course._id}`, 401))
+}
+
 
 await Course .deleteMany({ course: course._id})
 course.deleteOne()
